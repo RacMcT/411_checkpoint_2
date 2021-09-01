78e344da4a3a451d7424ace1274bd851f64b98ff
RacMcT <raccmct@gmail.com>
2021-09-01 12:54:12 -0500

    debugging

--- src/components/ListingPage.js
+++ src/components/ListingPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import red from "@material-ui/core/colors/red";
 import DeleteIcon from "@material-ui/icons/Delete";
