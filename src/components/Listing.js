faf0e5cb31cce2fd0cb1455be1757c27dd384a45
RacMcT <raccmct@gmail.com>
2021-09-06 16:42:53 -0500

    redoing listing component structure- table

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,20 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
 
-const Listing = (props) => {
-  const id = props.match.params.id;
-  let listing = props.listings.find((x) => x.id == id);
-  console.log(listing);
+import Delete from "@material-ui/icons/Delete"
+import { red } from "@material-ui/core/colors"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableContainer from "@material-ui/core/TableContainer"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Paper from "@material-ui/core/Paper"
+
+
+function Listing(props) {
+  useEffect(() => {
+    props.clearMarker()
+  }, [])
 
   return (
-    <div id="listingContainer">
+    <>
       <main>
-        <h1>{listing.name}</h1>
-        <p>{listing.address}</p>
-        <p>{listing.hours}</p>
-        <p>{listing.description}</p>
+        <TableContainer>
+          <Table style={{ minWidth: "650" }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontSize: "16px" }}>Name</TableCell>
+                <TableCell style={{ fontSize: "16px" }} align="left">
+                  Description
+                </TableCell>
+                <TableCell style={{ fontSize: "16px" }} align="left">
+                  Hours
+                </TableCell>
+                <TableCell style={{ fontSize: "16px" }} align="left">
+                  Address
+                </TableCell>
+                {props.checkAuth() ? (
+                  <TableCell style={{ fontSize: "16px" }} align="left">
+                    Delete
+                  </TableCell>
+                ) : null}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {props.listings.map(({ name, id, description, hours, address }, idx) => {
+                return (
+                  <TableRow key={name}>
+                    <TableCell style={{ fontSize: "16px" }} component="th" scope="row">
+                      <Link to={`/details/${id}`} className="details-link">
+                        {name}
+                      </Link>
+                    </TableCell>
+                    <TableCell style={{ fontSize: "16px" }} align="left">
+                      {description}
+                    </TableCell>
+                    <TableCell style={{ fontSize: "16px" }} align="left">
+                      {hours}
+                    </TableCell>
+                    <TableCell style={{ fontSize: "16px" }} align="left">
+                      {address}
+                    </TableCell>
+                    {props.checkAuth() ? (
+                      <TableCell style={{ fontSize: "16px" }} align="left">
+                        <Delete className="five" onClick={e => props.removeListing(idx)} style={{ color: red[500] }}></Delete>
+                      </TableCell>
+                    ) : null}
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </main>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default Listing;
+export default Listing
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,16 +1,45 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "redux"
 
-const loggedIn = (state = null) => state;
-
-const user = (state = [], action) => {
+const user = (state = null, action) => {
   switch (action.type) {
-    case "LOGIN":
-      //let userState = [...state];
-      console.log(state);
-      break;
+    case "ADD_USER":
+      let userState = [action.value]
+      return userState
+    case "REMOVE_USER":
+      let newState = [...state]
+      newState.splice(action.value, 1)
+      return newState
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default combineReducers({ loggedIn, user });
+const isLoggedIn = (state = false) => state
+const listings = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_LISTING":
+      return [action.value, ...state]
+    case "REMOVE_LISTING":
+      let newState = [...state]
+      newState.splice(action.value, 1)
+      return newState
+    default:
+      return state
+  }
+}
+
+const maps = (state = {}, action) => {
+  switch (action.type) {
+    case "FETCH_MARKER":
+      let mapState = [action.value]
+      return mapState
+    case "CLEAR_MARKER":
+      let newState = [state]
+      newState.splice(action.value, 1)
+      return newState
+    default:
+      return state
+  }
+}
+
+export default combineReducers({ user, isLoggedIn, listings, maps })
