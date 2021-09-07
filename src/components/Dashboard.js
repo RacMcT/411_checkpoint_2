ea700ae5903830e81e2489e719d9194926fec00c
RacMcT <raccmct@gmail.com>
2021-09-07 00:10:08 -0500

    working on http cookie

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,8 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Link from "react"
-
 import "./Dashboard.css"
+import { CookiesProvider } from "react-cookie";
 
 const checkAuth = () => {
   const cookies = cookie.parse(document.cookie)
